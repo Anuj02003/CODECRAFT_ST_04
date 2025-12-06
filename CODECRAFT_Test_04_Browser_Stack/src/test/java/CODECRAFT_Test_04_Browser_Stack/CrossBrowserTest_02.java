5189726701ed8ca4017bd4016ff92ef2048605a6
Anuj Patil <anujpatil1746@gmail.com>
2025-12-06 20:39:05 +0530

    Update CrossBrowserTest_02.java

--- CODECRAFT_Test_04_Browser_Stack/src/test/java/CODECRAFT_Test_04_Browser_Stack/CrossBrowserTest_02.java
+++ CODECRAFT_Test_04_Browser_Stack/src/test/java/CODECRAFT_Test_04_Browser_Stack/CrossBrowserTest_02.java
@@ -17,7 +17,6 @@ public class CrossBrowserTest_02 {
 	    public void loginTest() throws Exception {
 	        DesiredCapabilities caps = new DesiredCapabilities();
 
-	        // CHANGE BROWSERS HERE
 	        caps.setCapability("browserName", "Firefox");
 	        caps.setCapability("browserVersion", "latest");
 	        caps.setCapability("bstack:options", new java.util.HashMap<String, Object>() {{
